e004939758a51a991853a5cec7630902665aca17
Dan Lans <dslans@gmail.com>
2021-01-26 20:34:14 -0600

    testing one lane and progress with punishments

--- reward_function.py
+++ reward_function.py
@@ -16,8 +16,11 @@ def reward_function(params):
     all_wheels_on_track = params['all_wheels_on_track']
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    speed = params['speed']
-
+    speed = params['speed'] # 0-5.0 meters/sec
+    steps = params['steps'] # runs about 15 steps/sec
+    progress = params['progress'] # progress is 0-100 (percent)
+    is_left_of_center = params['is_left_of_center']
+    is_reversed = params['is_reversed'] # True=Clockwise
 
     # ------------------------------------------------------
     # Default Reward
@@ -25,30 +28,42 @@ def reward_function(params):
     reward  = 1.0
 
     # ------------------------------------------------------
-    # Keep the car on the track
+    # Keep the car on the track in one lane
     # ------------------------------------------------------
 
-    ## ------ Rewards ------
-    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
-        reward += 1.0
+    # Note: Staying in one lane may not be optimal route
 
-    ## ------ Punishments ------
+    # Stay on track
+    if all_wheels_on_track:
+        reward += 1.0 # reward for staying on track
 
-    # If the car goes off the track, assign zero reward
-    if not all_wheels_on_track:
-        reward = 1e-3
+        # track direction: clockwise
+        if is_reversed:
+            # penalize if it goes in the left lane
+            if is_left_of_center:
+                reward *= 0.5
+            else:
+                reward += 1.0 # additional reward for being in right lane
+        # track direction: counterclockwise
+        elif not is_reversed:
+            # penalize if it goes in the left lane
+            if not is_left_of_center:
+                reward *= 0.5
+            else:
+                reward += 1.0 # additional reward for being in left lane
+    # ------------------------------------------------------
+    # Finish the track
+    # ------------------------------------------------------
+
+    # Total number of steps to finish track (15 steps/sec)
+    TOTAL_NUM_STEPS = 300 # In the future, use track_length to estimate steps
+
+    # Give additional reward if the car pass every 100 steps faster than expected
+    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
+        reward += 10.0
 
     # ------------------------------------------------------
-    # Speed
-    # ------------------------------------------------------
-
-    # Penalize slow speed 
-    SPEED_THRESHOLD = 1.0 # threshold for low speed
-    if speed < SPEED_THRESHOLD:
-        reward *= 0.5
-
-    # ------------------------------------------------------
-    # Steering
+    # Obvious Punishments
     # ------------------------------------------------------
 
     # Penalize if car steer too much to prevent zigzag (range -30:30 degrees)
@@ -56,8 +71,14 @@ def reward_function(params):
     if steering > ABS_STEERING_THRESHOLD:
         reward *= 0.8
 
-    # ------------------------------------------------------
-    # Waypoints
-    # ------------------------------------------------------
+    # If the car goes off the track, assign zero reward
+    if not all_wheels_on_track:
+        reward = 1e-3
 
+    # Penalize slow speed
+    SPEED_THRESHOLD = 0.5 # threshold for low speed
+    if speed < SPEED_THRESHOLD:
+        reward *= 0.5
+
+    # ------ End of Function ------
     return float(reward)
